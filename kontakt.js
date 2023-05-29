5c8964e4cd3c698e2514eda60d35b7a6abc27a2c
lassebisgaard <Lasse_4321@hotmail.com>
2023-05-29 13:14:43 +0200

    Formular dpne

--- kontakt.js
+++ kontakt.js
@@ -1,16 +1,19 @@
-var loginForm = document.getElementById("loginForm");
-
-loginForm.addEventListener("submit", function(event) {
+var emailForm = document.getElementById("emailForm");
+emailForm.addEventListener("submit", function(event) {
   event.preventDefault();
-  
-  var username = document.getElementById("username").value;
-  var password = document.getElementById("password").value;
-  
-  // Her kan du tilføje din login-logik og validere brugernavn og adgangskode
-  // Her er et eksempel på en simpel validering, hvor brugernavn og adgangskode skal være udfyldt
-  if (username !== '' && password !== '') {
-    alert("Login succesfuldt!");
-  } else {
-    alert("Udfyld venligst både brugernavn og adgangskode.");
+
+  var email = document.getElementById("email").value;
+  var errorText = document.getElementById("errorText");
+
+  // Regex for e-mail validering
+  var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  if (emailRegex.test(email)) {
+    // E-mail er gyldig
+    errorText.innerHTML = '';
+    }
+   else {
+    // E-mail er ugyldig
+    errorText.innerHTML = 'Ugyldig e-mailadresse.';
   }
 });
