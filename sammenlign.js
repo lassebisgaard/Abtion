03165bb7798e2d9b803ff495486c3f64c538f0cf
Davidmms95 <121063495+Davidmms95@users.noreply.github.com>
2023-05-30 13:45:03 +0200

    søgefelt

---
+++ sammenlign.js
@@ -0,0 +1,25 @@
+     const biler = [
+            {
+                bilmodel: "Mercedes - Eqe 300 aut.",
+                Brændstof: "El",
+                Pris: "702.250 kr.",
+                CO2: "0 g/km",
+                Forbrug: "7,18 km/kWh",
+                kmh: "7,3 sek.",
+                Tophastighed: "210 km/t",
+                Rækkevidde: "630 km.",
+                Kapacitet: "87,7 kWh"
+            },
+            {
+                bilmodel: "Mercedes - C220 d aut. Advantage",
+                Brændstof: "Diesel",
+                Pris: "557.480 kr.",
+                CO2: "118 g/km",
+                Forbrug: "22,2 km/l",
+                kmh: "7,3 sek.",
+                Tophastighed: "245 km/t",
+                Rækkevidde: "1465,2 km.",
+                Kapacitet: "66L"
+            }
+        ];
+
