25845c35656b9f842c4eceb763f19303efee79fd
Davidmms95 <121063495+Davidmms95@users.noreply.github.com>
2023-05-31 00:06:16 +0200

    :(

--- sammenlign.js
+++ sammenlign.js
@@ -1,13 +1,13 @@
-    // Array of objects
+
     const cars = [
         {bilmodel: "Mercedes", brændstof: "Gasoline", Pris: "$10,000", CO2: "100 g/km", Forbrug: "8 L/100km", kmh: "180 km/h", Tophastighed: "220 km/h", Rækkevidde: "500 km", Kapacitet: "5 persons"},
         {bilmodel: "Mercedes", brændstof: "Gasoline", Pris: "$10,000", CO2: "100 g/km", Forbrug: "8 L/100km", kmh: "180 km/h", Tophastighed: "220 km/h", Rækkevidde: "500 km", Kapacitet: "5 persons"}
         ];
   
-      // Get the table element by its ID
+
       var table = document.getElementById("carTable");
   
-      // Create table header row
+
       var headerRow = table.insertRow();
       for (var property in cars[0]) {
         var th = document.createElement("th");
@@ -15,7 +15,7 @@
         headerRow.appendChild(th);
       }
   
-      // Create table rows for each car object
+
       for (var i = 0; i < cars.length; i++) {
         var car = cars[i];
         var row = table.insertRow();
