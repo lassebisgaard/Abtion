bef124b776273bb84884859e29dc6c5e82661161
lassebisgaard <Lasse_4321@hotmail.com>
2023-05-30 22:42:05 +0200

    e

--- kontakt.js
+++ kontakt.js
@@ -4,6 +4,7 @@ emailForm.addEventListener("submit", function(event) {
 
   var email = document.getElementById("email").value;
   var errorText = document.getElementById("errorText");
+  var messageText = document.getElementById("messageText");
 
   // Regex for e-mail validering
   var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -11,9 +12,20 @@ emailForm.addEventListener("submit", function(event) {
   if (emailRegex.test(email)) {
     // E-mail er gyldig
     errorText.innerHTML = '';
+
+    // Send e-mail og vis besked
+    for (var i = 0; i < 3; i++) {
+      sendEmail(email, i);
     }
-   else {
+    messageText.innerHTML = 'E-mailen er blevet sendt.'; // Vis beskedtekst
+  } else {
     // E-mail er ugyldig
     errorText.innerHTML = 'Ugyldig e-mailadresse.';
+    messageText.innerHTML = ''; // Nulstil beskedtekst
   }
 });
+
+function sendEmail(email, index) {
+  // Simulerer afsendelse af e-mail
+  console.log('E-mail ' + (index + 1) + ' sendt til: ' + email);
+}
