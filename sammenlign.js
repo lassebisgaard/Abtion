b2226efa7bc0ecf055f334ccff7e744cb436d36b
Davidmms95 <121063495+Davidmms95@users.noreply.github.com>
2023-05-30 22:45:29 +0200

    Tester af js

--- sammenlign.js
+++ sammenlign.js
@@ -1,25 +1,26 @@
-     const biler = [
-            {
-                bilmodel: "Mercedes - Eqe 300 aut.",
-                Brændstof: "El",
-                Pris: "702.250 kr.",
-                CO2: "0 g/km",
-                Forbrug: "7,18 km/kWh",
-                kmh: "7,3 sek.",
-                Tophastighed: "210 km/t",
-                Rækkevidde: "630 km.",
-                Kapacitet: "87,7 kWh"
-            },
-            {
-                bilmodel: "Mercedes - C220 d aut. Advantage",
-                Brændstof: "Diesel",
-                Pris: "557.480 kr.",
-                CO2: "118 g/km",
-                Forbrug: "22,2 km/l",
-                kmh: "7,3 sek.",
-                Tophastighed: "245 km/t",
-                Rækkevidde: "1465,2 km.",
-                Kapacitet: "66L"
-            }
+    // Array of objects
+    const cars = [
+        {bilmodel: "Mercedes", brændstof: "Gasoline", Pris: "$10,000", CO2: "100 g/km", Forbrug: "8 L/100km", kmh: "180 km/h", Tophastighed: "220 km/h", Rækkevidde: "500 km", Kapacitet: "5 persons"},
+        {bilmodel: "Mercedes", brændstof: "Gasoline", Pris: "$10,000", CO2: "100 g/km", Forbrug: "8 L/100km", kmh: "180 km/h", Tophastighed: "220 km/h", Rækkevidde: "500 km", Kapacitet: "5 persons"}
         ];
-
+  
+      // Get the table element by its ID
+      var table = document.getElementById("carTable");
+  
+      // Create table header row
+      var headerRow = table.insertRow();
+      for (var property in cars[0]) {
+        var th = document.createElement("th");
+        th.textContent = property;
+        headerRow.appendChild(th);
+      }
+  
+      // Create table rows for each car object
+      for (var i = 0; i < cars.length; i++) {
+        var car = cars[i];
+        var row = table.insertRow();
+        for (var property in car) {
+          var cell = row.insertCell();
+          cell.textContent = car[property];
+        }
+      }
