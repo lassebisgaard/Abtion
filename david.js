2c5847033909c019c53c3ae649a0111a350d41f1
Davidmms95 <121063495+Davidmms95@users.noreply.github.com>
2023-05-31 02:37:52 +0200

    Update david.js

--- david.js
+++ david.js
@@ -1,4 +1,4 @@
-const data = [
+let data = [
     { Bil: "Mercedes", Brændstof: "El", Pris: "702.250,-", CO2: "0 g/km", Forbrug: "7,18km/kWh", Acceleration: "0-100km/t - 7,3 sek.", Tophastighed: "210 km/t", Rækkevidde: "630 km.", Kapacitet: "87,7 kWh"},
     { Bil: "Honda", Brændstof: "Diesel", Pris: "557.480,-", CO2: "118 g/km", Forbrug: "22,2 km/l", Acceleration: "0-100km/t - 7,3 sek.", Tophastighed: "245 km/t", Rækkevidde: "1465,2 km.", Kapacitet: "66l" },
     { Bil: "Difference", Brændstof: "-", Pris: "144.770,-", CO2: "118 g/km", Forbrug: "-", Acceleration: "0-100km/t -", Tophastighed: "35 km/t", Rækkevidde: "835,2 km.", Kapacitet: "-" }
